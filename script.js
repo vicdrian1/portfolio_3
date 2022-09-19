76c2d409f2a02a42e59908d7ebf3cb31f7eac0f0
vicdrian1 <104452446+vicdrian1@users.noreply.github.com>
2022-09-19 23:36:46 +0200

    Index_Update

--- script.js
+++ script.js
@@ -44,5 +44,38 @@ $(document).ready(function () {
             $(this).stop(); 
         });
     })
-    
+
+    /*SCROLL*/
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $("#slide_nav").outerHeight();
+    // on scroll, let the interval function know the user has scrolled
+    $(window).scroll(function(event){
+    didScroll = true;
+    });
+    // run hasScrolled() and reset didScroll status
+    setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+    }, 250);
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+        if (Math.abs(lastScrollTop-st) <= delta)
+        return;
+        // If current position > last position AND scrolled past navbar...
+        if (st > lastScrollTop && st > navbarHeight){
+            // Scroll Down
+            $("#slide_nav").removeClass("nav-down").addClass("nav-up");
+        } else {
+            // Scroll Up
+            // If did not scroll past the document (possible on mac)...
+            if(st + $(window).height() < $(document).height()) { 
+            $("#slide_nav").removeClass("nav-up").addClass("nav-down");
+            }
+        }
+        lastScrollTop = st;
+    }   
 });
