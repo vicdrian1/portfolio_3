1a146ba9f4ac06a3333eb77abb369f56930f971c
vicdrian1 <104452446+vicdrian1@users.noreply.github.com>
2022-09-21 00:32:57 +0200

    gac updated

--- script.js
+++ script.js
@@ -45,42 +45,6 @@ $(document).ready(function () {
         });
     })
 
-    /*SCROLL*/
-/*     var didScroll;
-    var lastScrollTop = 0;
-    var delta = 5;
-    var navbarHeight = $("#slide_nav").outerHeight();
-    // on scroll, let the interval function know the user has scrolled
-    $(window).scroll(function(event){
-    didScroll = true;
-    });
-    // run hasScrolled() and reset didScroll status
-    setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-    }, 250);
-    function hasScrolled() {
-        var st = $(this).scrollTop();
-        if (Math.abs(lastScrollTop-st) <= delta)
-        return;
-        // If current position > last position AND scrolled past navbar...
-        if (st > lastScrollTop && st > navbarHeight){
-            // Scroll Down
-            $("#slide_nav").removeClass("nav-down").addClass("nav-up");
-            $("#slide_nav").slideUp(400);
-        } else {
-            // Scroll Up
-            // If did not scroll past the document (possible on mac)...
-            if(st + $(window).height() < $(document).height()) { 
-            $("#slide_nav").removeClass("nav-up").addClass("nav-down");
-            $("#slide_nav").slideDown(400);
-            }
-        }
-        lastScrollTop = st;
-    }    */
-
     var lastScrollTop = 0;
     var headerHeight = $("header").outerHeight();
     var menuHeight = $("#slide_nav").outerHeight();
